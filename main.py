2db494c023d79c1c454f76322d9a5d2c83b6532e
voxj <129677054+voxj@users.noreply.github.com>
2023-04-29 17:39:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import discord
 import openai 
+import os
 from discord.ext import commands 
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='',intents=intents)
-token = "" # Discord bot token 
+token = os.getenv("put your env name here") # Discord Bot Token 
 
-channel_id = 0 # Channel id 
-msgs = [{"role":"system",'content':'You are a chatbot that is like chatgpt and that help people.'}]
+channel_id = 0 # Channel ID 
+msgs = [{"role":"system",'content':'You are a helpful assistant based on gpt-3.5-turbo. Your name is ChatGPT.'}]
 def chat(c):
     global msgs 
-    openai.api_key = "" # Openai token
+    openai.api_key = os.getenv("put your env name here") # OpenAI API Key
     msgs.append({"role":"user","content":c})
     cbot = openai.ChatCompletion.create(model='gpt-3.5-turbo',messages=msgs)
     msgs.append(cbot['choices'][0]['message'])
@@ -26,5 +27,5 @@ async def on_message(msg):
         if msg.channel.id == channel_id:
             a = await msg.channel.send('**Please wait...**')
             res = chat(msg.content)
-            await a.edit(content='',embed=discord.Embed(title='Chatgpt :',description=f'**This uses openai api**\n> {msg.content}\n{res}'))
+            await a.edit(content='',embed=discord.Embed(title='GPT:',description=f'**This uses OpenAI API.**\n> {msg.content}\n{res}'))
 client.run(token)
